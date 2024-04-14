0cd3fdf73a4d7ea7fe2c029673f63bcb360944cf
ShamNeko <mkdvlp01@gmail.com>
2024-04-15 00:15:31 +0900

    ゲームをする場所以外のモックはある程度できた

--- janken/__main__.py
+++ janken/__main__.py
@@ -1,6 +1,5 @@
 import tkinter as tk
 from tkinter import ttk
-from data import data_test as dtest
 
 class Application(tk.Frame):
     def __init__(self, master=None):
@@ -12,17 +11,21 @@ class Application(tk.Frame):
         # ウィジェットの配置
         self.set_widgets()
 
+    # ウィジェットの配置
     def set_widgets(self):
         # ******** ヘッダーゾーン *************
         # ヘッダーフレーム作成
         self.header_zone = tk.Frame(self.master)
         self.header_zone.config(width=1000, height=50, background="gray")
         self.header_zone.grid(row=0, column=0, columnspan=2)
+        # ユーザーの管理
+        self.users_btn = ttk.Button(self.header_zone, text="ユーザー管理", command=self.user_manager)
+        self.users_btn.place(x=30, y=10)
         # ログインボタン
-        self.login_btn = ttk.Button(self.header_zone, text="ログイン")
+        self.login_btn = ttk.Button(self.header_zone, text="ログイン", command=self.login)
         self.login_btn.place(x=800, y=10)
         # ログインユーザー名
-        self.login_user_name = ttk.Label(self.header_zone, text="test_user1", font=10)
+        self.login_user_name = ttk.Label(self.header_zone, text="test_user1", font=12)
         self.login_user_name.place(x=880, y=10)
 
         # ******** ゲームゾーン *************
@@ -46,12 +49,28 @@ class Application(tk.Frame):
         self.upd_score_btn = ttk.Button(self.score_zone, text="更新", command=self.upd_score)
         self.upd_score_btn.place(x=55, y=5)
         # プレイ回数
-        self.play_cnt = ttk.Label(self.score_zone, text=f"プレイ回数：{dtest.test1}")
+        self.play_cnt = ttk.Label(self.score_zone, text=f"プレイ回数：")
         self.play_cnt.place(x=10, y= 50)
+        # 勝利数
+        self.win_cnt = ttk.Label(self.score_zone, text="勝利数：")
+        self.win_cnt.place(x=10, y=70)
+        # 敗北数
+        self.lose_cnt = ttk.Label(self.score_zone, text="敗北数：")
+        self.lose_cnt.place(x=10, y=90)
+        # あいこ数
+        self.draw_cnt = ttk.Label(self.score_zone, text="あいこ数：")
+        self.draw_cnt.place(x=10, y=110)
+
+    def user_manager(self):
+        print("ユーザーの管理")
+
+    # ログイン処理
+    def login(self):
+        print("ログイン")
 
     # 成績更新処理
     def upd_score(self):
-        print("更新")
+        print("成績更新")
 
     # EXCEL出力
     def export_excel(self):
